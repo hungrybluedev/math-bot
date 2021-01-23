e712f85e7f09d304dfd1c856971c12b2fc8bd2f8
Subhomoy Haldar <hungrybluedev@gmail.com>
2021-01-24 00:45:54 +0530

    add uniform and char limit mechanism

--- mathbot/commands.py
+++ mathbot/commands.py
@@ -1,3 +1,5 @@
+from mathbot.main import COMPACT_FORMAT, DOCUMENTATION_LINK
+import random
 import statistics as stats
 
 
@@ -64,7 +66,64 @@ def _pstdev(args):
     return stats.pstdev(num_arr)
 
 
+def _convert_to_positive_int(num_str):
+    try:
+        value = int(num_str)
+        if value > 0:
+            return min(value, GENERATOR_LIMIT)
+        else:
+            raise ValueError("%d is not positive.")
+    except:
+        raise ValueError(
+            "Expected a positive integer. Got %s instead." % num_str)
+
+
+def _uniform(args):
+    n = len(args)
+    if n == 0:
+        # 1. No arguments provided; return 1 uniform variate from [0, 1)
+        return random.uniform(0, 1)
+    elif n == 1:
+        # 2. One argument provided - count; ensure that it is positive
+        # return "count" uniform variates from [0, 1)
+        count = _convert_to_positive_int(args[0])
+        return " ".join([str(COMPACT_FORMAT % random.uniform(0, 1)) for _ in range(count)])
+    elif n == 2:
+        # 3. Two arguments provided - upper (float) and count (positive integer)
+        lower = 0
+        upper = _convert_to_floats([args[0]])[0]
+        # Ensure that lower < upper
+        if upper < lower:
+            temp = lower
+            lower = upper
+            upper = temp
+        count = _convert_to_positive_int(args[1])
+        return " ".join([str(COMPACT_FORMAT % random.uniform(lower, upper)) for _ in range(count)])
+    elif n == 3:
+        # 4. Three arguments provided - lower (float), upper (float) and count (positive integer)
+        bound = _convert_to_floats(args[0:2])
+        lower = bound[0]
+        upper = bound[1]
+        # Ensure that lower < upper
+        if upper < lower:
+            temp = lower
+            lower = upper
+            upper = temp
+        count = _convert_to_positive_int(args[2])
+        return " ".join([str(COMPACT_FORMAT % random.uniform(lower, upper)) for _ in range(count)])
+    else:
+        raise ValueError(
+            "Incorrect number of arguments. Refer to the documentation: %s" % DOCUMENTATION_LINK)
+
+
 _function_map = {
+    # ========================
+    # DISTRIBUTION SIMULATIONS
+    # ========================
+    "uniform": _uniform,
+    # ==========
+    # STATISTICS
+    # ==========
     "mean": _mean,
     "median": _median,
     "mode": _mode,
